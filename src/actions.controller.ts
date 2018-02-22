b1ab6148238bac317715ccb95e2f947004971596
Vincent Rasquier <vrasquier@keley-live.com>
2018-02-22 14:34:58 +0100

    Regroup action by sens

--- src/actions.controller.ts
+++ src/actions.controller.ts
@@ -5,32 +5,4 @@ import { EngineService } from "./service/engine.service";
 
 @ApiUseTags("actions")
 @Controller("actions")
-export class ActionsController {
-  constructor(private readonly engine: EngineService) {}
-
-  @ApiOperation({
-    title: "Make card play"
-  })
-  @ApiResponse({
-    status: 200,
-    description: "Good",
-    type: StateDTO
-  })
-  @Post("card-play")
-  cardPlay(@Body() state: StateDTO) {
-    return this.engine.getEngine().playCard(state);
-  }
-
-  @ApiOperation({
-    title: "Make card distribution"
-  })
-  @ApiResponse({
-    status: 200,
-    description: "Good",
-    type: StateDTO
-  })
-  @Post("card-distribute")
-  cardDistribute(@Body() state: StateDTO) {
-    return this.engine.getEngine().distributeCards(state);
-  }
-}
+export class ActionsController {}
--- src/cards.controller.ts
+++ src/cards.controller.ts
@@ -1,12 +1,17 @@
-import { Get, Controller, Param, Query } from "@nestjs/common";
+import { Get, Controller, Param, Query, Post, Body } from "@nestjs/common";
 import { ApiUseTags, ApiResponse, ApiOperation } from "@nestjs/swagger";
-import { CardDTO } from "./dto/card.dto";
 import { CardService } from "./service/card.service";
+import { EngineService } from "./service/engine.service";
+import { CardDTO } from "./dto/card.dto";
+import { StateDTO } from "./dto/state.dto";
 
 @ApiUseTags("cards")
 @Controller("cards")
 export class CardsController {
-  constructor(private readonly service: CardService) {}
+  constructor(
+    private readonly service: CardService,
+    private readonly engine: EngineService
+  ) {}
 
   @ApiOperation({
     title: "Find one card by uid"
@@ -19,5 +24,31 @@ export class CardsController {
   @Get(":uid")
   findOne(@Param("uid") uid: string): CardDTO {
     return this.service.find(uid);
+  }
+
+  @ApiOperation({
+    title: "Make card play"
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Good",
+    type: StateDTO
+  })
+  @Post("play")
+  cardPlay(@Body() state: StateDTO) {
+    return this.engine.getEngine().playCard(state);
+  }
+
+  @ApiOperation({
+    title: "Make cards distribution"
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Good",
+    type: StateDTO
+  })
+  @Post("distribute")
+  cardDistribute(@Body() state: StateDTO) {
+    return this.engine.getEngine().distributeCards(state);
   }
 }
