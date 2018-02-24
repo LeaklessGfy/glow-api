b25fcf8d2db75879590d5803cd9de04e4e74e909
Leakless <biokille77@gmail.com>
2018-02-24 12:34:21 +0100

    Update modes

--- src/service/mode.service.ts
+++ src/service/mode.service.ts
@@ -1,18 +1,11 @@
 import { Component } from "@nestjs/common";
 import { ModeDTO } from "../dto/mode.dto";
-//import * as M from "../../wlw-engine/src/resources/modes";
+import * as M from "../../wlw-engine/src/resources/modes";
 
 @Component()
 export class ModeService {
   private readonly modes: ModeDTO[] = [
-    {
-      uid: "Normal2Versus",
-      name: "Normal",
-      img:
-        "http://cdn1.thecomeback.com/wp-content/uploads/2017/01/roman-reigns-goldberg-wwe-raw.jpg",
-      description: "One vs. One",
-      numbers: 2
-    }
+    M.Normal
   ];
 
   findAll(): ModeDTO[] {
