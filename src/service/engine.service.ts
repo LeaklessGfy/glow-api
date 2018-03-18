c67296b01180c73f075196b72824a3e53986905b
Leakless <biokille77@gmail.com>
2018-03-18 17:41:17 +0100

    Update core

--- src/service/engine.service.ts
+++ src/service/engine.service.ts
@@ -1,28 +1,29 @@
 import { Component } from "@nestjs/common";
-import Kernel from "../../wlw-engine/src/models/kernel";
 import Engine from "../../wlw-engine/src/models/engine";
-import CoreKernel from "../../wlw-engine/src/kernel";
 import CoreEngine from "../../wlw-engine/src/engine";
+import * as Strategies from "../../wlw-engine/src/resources/strategies";
 import * as Actuators from "../../wlw-engine/src/resources/actuators";
+import Mutator from "../../wlw-engine/src/mutator";
 
 @Component()
 export class EngineService {
-  private readonly kernel: Kernel;
   private readonly engine: Engine;
 
   constructor() {
-    this.kernel = new CoreKernel([
-      new Actuators.DamageActuator(),
-      new Actuators.MirrorActuator(),
-      new Actuators.PinActuator(),
-      new Actuators.RestActuator(),
-      new Actuators.TimewrapActuator()
-    ]);
-    this.engine = new CoreEngine(this.kernel);
-  }
-
-  getKernel(): Kernel {
-    return this.kernel;
+    const mutator = new Mutator(
+      new Strategies.Distributor(),
+      new Strategies.Validator(),
+      new Strategies.Operator([
+        new Actuators.DamageActuator(),
+        new Actuators.MirrorActuator(),
+        new Actuators.PinActuator(),
+        new Actuators.RestActuator(),
+        new Actuators.TimewrapActuator()
+      ]),
+      new Strategies.CPU(),
+      new Strategies.Winning()
+    );
+    this.engine = new CoreEngine(mutator);
   }
 
   getEngine(): Engine {
